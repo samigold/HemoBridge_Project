afd1f81952b96575361325088d6e02e3c94d0ea6
Samuel Madu <info@samuelmadu.com>
2025-03-22 18:57:01 +0100

    fix: dynamically set secure cookie flag based on environment

--- utils/generateTokenAndSetCookie.js
+++ utils/generateTokenAndSetCookie.js
@@ -8,9 +8,8 @@ export const generateTokenAndSetCookie = (res, userId) => {
 res.cookie('jwt', token, {
     httpOnly: true,
     maxAge: 7 * 60 * 60 * 1000, //7 hours,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: "strict",
-    maxAge: 7 * 60 * 60 * 1000 //7 hours
+    secure: process.env.NODE_ENV === 'production'? true: false,
+    sameSite: "none",
 })
 
     return token;
