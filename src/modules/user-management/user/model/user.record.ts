1132a3d5f3b91658c1e162d081e628ec0562f679
Keenlabi <keenlabinc@gmail.com>
2025-04-03 06:25:47 +0100

    removed first_name and last_name from the user type

--- src/modules/user-management/user/model/user.record.ts
+++ src/modules/user-management/user/model/user.record.ts
@@ -3,15 +3,14 @@ import { Types } from "mongoose";
 export interface UserRecord {
     _id?: Types.ObjectId;
     id: string;
-    first_name: string;
-    last_name: string;
-    phone_number: string;
-    blood_type: string;
     is_active: boolean;
     is_suspended: boolean;
     email: string;
+    phone_number: string;
+    blood_type: string;
     password_hash: string;
     role: string;
+    last_seen: Date;
     created_at: Date;
     updated_at: Date;
 }
