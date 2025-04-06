40cd4bc780989969d24e49082a36f99c75fca79d
Keenlabi <keenlabinc@gmail.com>
2025-04-07 00:03:15 +0100

    removed unused imports

--- src/modules/user-management/user/user.routes.ts
+++ src/modules/user-management/user/user.routes.ts
@@ -1,20 +1,10 @@
 import express from 'express';
 import { UserController } from './user.controller';
-// import { authorize } from '../middleware/authorize';
 
 const UserRoutes = express.Router();
 
 UserRoutes.post("/register/care-giver", UserController.registerCareGiver);
 UserRoutes.post("/register/donor", UserController.registerDonor);
 UserRoutes.post("/register/admin", UserController.registerAdmin);
-
-// UserRoutes.post("/register/caregiver", UserController.registerCareGiver);
-
-// User Management Routes
-// router.get('/users', authorize, checkRole(['admin']), UserController.getAllUsers);  // get all users (admin only)
-// UserRoutes.get('/users/:userId', authMiddleware, UserController.getUserById); // get user by id (admin, user)
-// UserRoutes.put('/users/:userId', authMiddleware, UserController.updateUser); // update user by id (admin, user)
-// UserRoutes.delete('/users/:userId', authMiddleware, checkRole(['admin']), UserController.deleteUser);
-// UserRoutes.put('/users/:userId/status', authMiddleware, checkRole(['admin']), UserController.updateUserStatus);
 
 export default UserRoutes;
