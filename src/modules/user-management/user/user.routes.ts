ec08bf6c6ae01e7f828977591afe9ce5783947a2
Keenlabi <keenlabinc@gmail.com>
2025-04-06 15:19:16 +0100

    created new route for register care giver

--- src/modules/user-management/user/user.routes.ts
+++ src/modules/user-management/user/user.routes.ts
@@ -4,6 +4,7 @@ import { UserController } from './user.controller';
 
 const UserRoutes = express.Router();
 
+UserRoutes.post("/register/care-giver", UserController.registerCareGiver);
 UserRoutes.post("/register/donor", UserController.registerDonor);
 UserRoutes.post("/register/admin", UserController.registerAdmin);
 
