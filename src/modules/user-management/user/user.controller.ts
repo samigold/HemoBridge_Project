01f1c7dd6d95075943aca07714b4f8f50889c82e
Keenlabi <keenlabinc@gmail.com>
2025-04-07 00:02:07 +0100

    created the register facility staff controller function

--- src/modules/user-management/user/user.controller.ts
+++ src/modules/user-management/user/user.controller.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
 import { UserService } from "./user.service";
-import { UserRoles } from "./model/user.record";
 import eventBus from "src/shared/events/event-bus";
 import { USER_EVENTS } from "src/shared/events/user.events";
+import { USER_ROLE } from "src/shared/constants/user-role.enum";
 
 export const UserController = {
     registerDonor: async (req: Request, res: Response) => {
@@ -18,7 +18,7 @@ export const UserController = {
             email,
             password,
             phone_number: phoneNumber,
-            role: UserRoles.DONOR
+            role: USER_ROLE.DONOR
 
         }).catch(()=> {
             throw new Error('Invalid donor data. Error creating Donor');
@@ -31,7 +31,7 @@ export const UserController = {
             blood_type: bloodType,
             address: address,
             phone_number: phoneNumber,
-            role: UserRoles.DONOR,
+            role: USER_ROLE.DONOR,
         });
         
         delete createdUserEntity.passwordHash
@@ -56,7 +56,7 @@ export const UserController = {
             email,
             password,
             phone_number: phoneNumber,
-            role: UserRoles.CARE_GIVER
+            role: USER_ROLE.CARE_GIVER
         })
         .catch(()=> {
             throw new Error('Invalid caregiver data. Error creating caregiver');
@@ -68,12 +68,49 @@ export const UserController = {
             last_name: lastName,
             phone_number: phoneNumber,
             address: address,
-            role: UserRoles.CARE_GIVER,
+            role: USER_ROLE.CARE_GIVER,
         });
 
         res.status(201).json({
             success: true,
             message: "New care giver user created successfully"
+        });
+    },
+
+    registerFacilityStaff: async (req: Request, res: Response) => {
+
+        const { facilityId, firstName, lastName, email, password, phoneNumber, address } = req.body;
+
+        if (!facilityId || !firstName || !lastName || !email || !password || !phoneNumber || !address) {
+            res.status(400);
+            throw new Error('All fields are required');
+        }
+
+        const createdUserEntity = await UserService.create({
+            email,
+            password,
+            phone_number: phoneNumber,
+            role: USER_ROLE.FACILITY_STAFF
+
+        }).catch(()=> {
+            throw new Error('Invalid donor data. Error creating Donor');
+        })
+
+        eventBus.emit(USER_EVENTS.CREATED, {
+            user_id: createdUserEntity.id,
+            first_name: firstName,
+            last_name: lastName,
+            address: address,
+            phone_number: phoneNumber,
+            role: USER_ROLE.FACILITY_STAFF,
+        });
+        
+        delete createdUserEntity.passwordHash
+        
+        res.status(201).json({
+            success: true,
+            message: "New donor user created successfully",
+            user: createdUserEntity
         });
     },
 
