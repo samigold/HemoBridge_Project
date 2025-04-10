674b36ab2f0726d0f55339a66a3faba56761065a
Keenlabi <keenlabinc@gmail.com>
2025-04-10 12:04:33 +0100

    removed the implementation for the facility staff controller create function since a creating a facility staff user is now handled in the user domain and by extention the facility staff service, no need for the controller here

--- src/modules/health-care-facility/facility-staff/facility-staff.controller.ts
+++ src/modules/health-care-facility/facility-staff/facility-staff.controller.ts
@@ -1,32 +1,7 @@
 import { Request, Response } from "express";
-import { ValidationError } from "src/shared/errors";
-import { FacilityStaffService } from "./facility-staff.service";
 
 export const FacilityStaffController = {
     create: async(req: Request, res: Response)=> {
-
-        const { name, address, email, phoneNumber, operationalHours } = req.body;
-
-        if(!name || !address || !email || !phoneNumber || !operationalHours) {
-            throw new ValidationError("All fields are required");
-        }
-
-        const createdFacilityRecord = await FacilityStaffService.create({
-            name,
-            address,
-            email,
-            phoneNumber,
-            operationalHours
         
-        }).catch((error)=> {
-            console.error("There was an error creating health care facility: ", error);
-            throw error
-        })
-
-        res.status(201).json({
-            success: true,
-            message: "New health care facility created successfully",
-            data: createdFacilityRecord
-        });
     }
 }
