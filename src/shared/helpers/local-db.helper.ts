ea23be5b0e7cbc7d7b1f5b039fefd080d7e2b753
Keenlabi <keenlabinc@gmail.com>
2025-04-27 13:40:28 +0100

    feat: implement local database helper for fetching blood inventory data

---
+++ src/shared/helpers/local-db.helper.ts
@@ -0,0 +1,23 @@
+import { DonorBloodTypes } from 'src/modules/user/donor/model/donor.record';
+import { loadJsonFromFile } from '../utils/load-json-from-file';
+
+interface ILocalDB {
+    facilities: [{
+        id: string;
+        name: string;
+        address: string;
+        bloodType: DonorBloodTypes[];
+        urgency: string;
+        stock: [{
+            type: DonorBloodTypes;
+            stock: number;
+            expiry: string;
+        }];
+        hours: string;
+    }]
+}
+
+export async function fetchLocalDB(): Promise<ILocalDB> {
+    const db = await loadJsonFromFile<ILocalDB>("data/db.json");
+    return db;
+}
