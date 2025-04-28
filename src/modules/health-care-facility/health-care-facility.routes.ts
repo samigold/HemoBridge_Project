1f26a873377eedfcc3a0425d23a7bbcc405f2dbf
Keenlabi <keenlabinc@gmail.com>
2025-04-28 18:55:38 +0100

    feat: add blood inventory route to health care facility router

--- src/modules/health-care-facility/health-care-facility.routes.ts
+++ src/modules/health-care-facility/health-care-facility.routes.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import FacilityRoutes from './base/facility.routes';
 import FacilityStaffRoutes from './facility-staff/facility-staff.routes';
+import BloodInventoryRoutes from './blood-inventory/blood-inventory.routes';
 
 const HealthCareFacilityRouter = express.Router();
 
 HealthCareFacilityRouter.use('/facility', FacilityRoutes);
+HealthCareFacilityRouter.use('/facility/staff', FacilityStaffRoutes);
 HealthCareFacilityRouter.use('/facility/:facilityId/staff', FacilityStaffRoutes);
-HealthCareFacilityRouter.use('/facility/:facilityId/staff', FacilityStaffRoutes);
+HealthCareFacilityRouter.use('/facility/:facilityId/inventory', BloodInventoryRoutes);
 
 export default HealthCareFacilityRouter;
