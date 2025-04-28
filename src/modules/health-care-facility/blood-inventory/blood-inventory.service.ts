365fd55151622a838b62bfd38570ed4db3ae32ca
Keenlabi <keenlabinc@gmail.com>
2025-04-28 12:08:34 +0100

    feat: implement findByFacilityId method in BloodInventoryService to retrieve inventory records by facility ID

--- src/modules/health-care-facility/blood-inventory/blood-inventory.service.ts
+++ src/modules/health-care-facility/blood-inventory/blood-inventory.service.ts
@@ -4,6 +4,16 @@ import { BloodInventoryModel } from "./model/blood-inventory.model"
 import { BloodInventoryEntity } from "./blood-inventory.entity"
 
 export const BloodInventoryService = {
+    findByFacilityId: async (facilityId: string)=> {
+        const foundInventoryRecords = await BloodInventoryModel.find({ facility_id: facilityId })
+        .catch((error)=> { 
+            console.error("There was an error finding blood inventory record: ", error);
+            throw new InternalServerError("")
+        })
+
+        return  foundInventoryRecords.map(record => BloodInventoryEntity.fromRecord(record))
+    },
+
     create: async (newBloodInventory: ICreateBloodInventory)=> {
         const createdRecord = await BloodInventoryModel.create({
             facility_id: newBloodInventory.facilityId,
