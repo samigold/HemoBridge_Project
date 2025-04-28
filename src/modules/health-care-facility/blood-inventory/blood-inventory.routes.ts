859697041932aeef661974b1a38c8f85a383e5d3
Keenlabi <keenlabinc@gmail.com>
2025-04-28 18:54:57 +0100

    fix: remove access validation from blood inventory fetch route

--- src/modules/health-care-facility/blood-inventory/blood-inventory.routes.ts
+++ src/modules/health-care-facility/blood-inventory/blood-inventory.routes.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 import { validateSession } from 'src/shared/middleware/validate-session.middleware';
-import { validateAccess } from 'src/shared/middleware/access.middleware';
-import { USER_ROLE } from 'src/shared/constants/user-role.enum';
 import { applyAsyncHandler } from 'src/shared/middleware/async-handler.middleware';
 import { BloodInventoryController } from './facility-staff.controller';
 
 const BloodInventoryRoutes = express.Router({ mergeParams: true });
 
-BloodInventoryRoutes.get("/", validateSession, validateAccess(USER_ROLE.ADMIN), BloodInventoryController.fetchAll);
+BloodInventoryRoutes.get("/", validateSession, BloodInventoryController.fetchAll);
 
 applyAsyncHandler(BloodInventoryRoutes);
 export default BloodInventoryRoutes;
