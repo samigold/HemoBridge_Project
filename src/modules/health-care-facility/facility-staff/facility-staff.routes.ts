b88810daa7a8117356a9b4d1b674562ce6c63f46
Keenlabi <keenlabinc@gmail.com>
2025-04-28 18:55:49 +0100

    fix: simplify facility staff fetch route by removing access validation

--- src/modules/health-care-facility/facility-staff/facility-staff.routes.ts
+++ src/modules/health-care-facility/facility-staff/facility-staff.routes.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 import { validateSession } from 'src/shared/middleware/validate-session.middleware';
-import { validateAccess } from 'src/shared/middleware/access.middleware';
-import { USER_ROLE } from 'src/shared/constants/user-role.enum';
 import { applyAsyncHandler } from 'src/shared/middleware/async-handler.middleware';
 import { FacilityStaffController } from './facility-staff.controller';
 
 const FacilityStaffRoutes = express.Router({ mergeParams: true });
 
-FacilityStaffRoutes.get("/:page", validateSession, validateAccess(USER_ROLE.ADMIN), FacilityStaffController.fetch);
+FacilityStaffRoutes.get("", validateSession, FacilityStaffController.fetch);
 
 applyAsyncHandler(FacilityStaffRoutes);
 export default FacilityStaffRoutes;
