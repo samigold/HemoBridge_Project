0d5ab721a1392f822c56ee9b45dd6f738975edb6
Samuel Madu <info@samuelmadu.com>
2025-05-01 14:03:53 +0100

    fix:  rolled back Allowed donors to be able to cancel an already scheduled donation

--- src/modules/donation/schedule/donation-schedule.routes.ts
+++ src/modules/donation/schedule/donation-schedule.routes.ts
@@ -15,7 +15,7 @@ DonationScheduleRouter.patch('/:donationScheduleId/assign', validateSession, val
 // Facility routes
 DonationScheduleRouter.get('/facility', validateSession, validateAccess(USER_ROLE.FACILITY_STAFF), DonationScheduleController.fetchFacilitySchedules);
 DonationScheduleRouter.post('/:donationScheduleId/approve', validateSession, validateAccess(USER_ROLE.FACILITY_STAFF), DonationScheduleController.approve);
-DonationScheduleRouter.post('/:donationScheduleId/decline', validateSession, validateAccess(USER_ROLE.DONOR,USER_ROLE.FACILITY_STAFF), DonationScheduleController.decline);
+DonationScheduleRouter.post('/:donationScheduleId/decline', validateSession, validateAccess(USER_ROLE.FACILITY_STAFF), DonationScheduleController.decline);
 DonationScheduleRouter.post('/:donationScheduleId/complete', validateSession, validateAccess(USER_ROLE.FACILITY_STAFF), DonationScheduleController.complete);
 
 applyAsyncHandler(DonationScheduleRouter);
