8f8573acb0523bfdd78ac7344e7cc9ad1d45c5e0
Keenlabi <keenlabinc@gmail.com>
2025-05-01 07:26:29 +0100

    populating the facilityId with facility details if exists

--- src/modules/donation/schedule/donation-schedule.entity.ts
+++ src/modules/donation/schedule/donation-schedule.entity.ts
@@ -1,17 +1,25 @@
 import { Types } from "mongoose";
 import { DonorBloodTypes } from "src/modules/user/donor/model/donor.record";
 import { DonationScheduleRecord, DonationScheduleStatus, DonationScheduleUrgency } from "./model/donation-schedule.record";
+import { FacilityEntity } from "src/modules/health-care-facility/base/facility.entity";
 
 interface PopulatedDonor {
     firstName: string;
     lastName: string;
 }
 
+interface PopulatedFacility {
+    name: string;
+    address: string;
+    operationalHours: string;
+    bloodTypes: DonorBloodTypes[];
+}
+
 export interface DonationScheduleEntity {
     _id?: Types.ObjectId;
     id?: string;
     donorId?: string | PopulatedDonor;
-    facilityId: string;
+    facilityId?: string | PopulatedFacility;
     bloodType: DonorBloodTypes;
     unitsRequested: number;
     additionalNotes?: string;
@@ -26,18 +34,29 @@ export const DonationScheduleEntity = {
     fromRecord(record: DonationScheduleRecord): DonationScheduleEntity {
         const donor = record.donor_id 
             ?   typeof record.donor_id === 'string' 
-                ? record.donor_id 
-                : {
-                    firstName: record.donor_id.first_name!,
-                    lastName: record.donor_id.last_name!
-                }
+                ?   record.donor_id 
+                :   {
+                        firstName: record.donor_id.first_name!,
+                        lastName: record.donor_id.last_name!
+                    }
+            :   undefined
+
+        const facility = record.facility_id 
+            ?   typeof record.facility_id === 'string' 
+                ?   record.facility_id
+                :   {
+                        name: record.facility_id.name!,
+                        address: record.facility_id.address!,
+                        operationalHours: record.facility_id.operational_hours!,
+                        bloodTypes: record.facility_id.blood_types!,
+                    }
             :   undefined
 
 
         return {
             id: record.id,
             donorId: donor,
-            facilityId: record.facility_id,
+            facilityId: facility,
             bloodType: record.blood_type,
             unitsRequested: record.units_requested,
             additionalNotes: record.additional_notes,
